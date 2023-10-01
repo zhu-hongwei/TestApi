e100667c7453c84e1f5bd616069b77a141c3c310
zhu-hongwei <root@hongweizhu.com>
2023-10-02 01:26:40 +0800

    Initial Commit

---
+++ TestApi/TestApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApiApp.swift
+//  TestApi
+//
+//  Created by 朱洪苇 on 2023/10/2.
+//
+
+import SwiftUI
+
+@main
+struct TestApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
